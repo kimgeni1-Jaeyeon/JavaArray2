b6b38e69d09af5fc5e0e60d1a79d98ea42db5b72
unknown <kimgeni1@khu.ac.kr>
2024-09-16 22:02:54 +0900

    ver4

--- src/ReserveDriver.java
+++ src/ReserveDriver.java
@@ -1,9 +1,24 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.time.format.DateTimeFormatter; // 시간 단위 설정
+import java.time.format.DateTimeParseException; // 직접 시간 입력받는데에 필요
+
+
+class ReserveInfo implements Comparable{
+    String name;
+    LocalDateTime now;
+
+    @Override
+    public int compareTo(Object o) {
+        if ( this.now.isEqual(((ReserveInfo)o).now)) return 0;
+        else if ( this.now.isBefore(((ReserveInfo)o).now)) return -1;
+        else return 1;
+    }
+}
 
 class Reserve {
-    List<String>[] seat2 = new LinkedList[10];
+    List<ReserveInfo>[] seat2 = new LinkedList[10];
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"); // 분단위까지만 표시
 
     public Reserve() {
         for (int i = 0; i < 10; i++) {
@@ -19,15 +34,53 @@ class Reserve {
         while (true) {
             System.out.print("원하는 좌석번호 입력: ");
             int a = sc.nextInt();
-            System.out.print("이름 입력: ");
-            String name = sc.next();
 
-            if (seat2[a - 1].isEmpty()) {  //왜 == null은 안되지?
-                seat2[a - 1].add(name);
+            ReserveInfo RInfo = new ReserveInfo();
+
+            System.out.print("이름 입력: ");
+            RInfo.name = sc.next();
+
+            System.out.print("예약 시간 설정(0은 현재시간, 1은 직접입력): ");
+            int b = sc.nextInt();
+            sc.nextLine();
+
+            if (b == 0)
+            {RInfo.now = LocalDateTime.now();}
+            else {
+                LocalDateTime dateTime = null;
+                while (true) {
+                    System.out.print("예약 시간 입력 (형식: yyyy-MM-dd HH:mm): ");
+                    String input = sc.nextLine();//공백도 포함
+                    try {
+                        dateTime = LocalDateTime.parse(input, formatter);
+                        break;
+                    } catch (DateTimeParseException e) {
+                        System.out.println("잘못된 입력 형식입니다. 다시 시도해 주세요.");
+                    }
+                }
+                RInfo.now = dateTime;
+
+            }
+
+            boolean isSameTime = false; //이거 에러 보자
+            for (ReserveInfo existingInfo : seat2[a - 1]) {
+                if (existingInfo.now.format(formatter).equals(RInfo.now.format(formatter))) {
+                    System.out.println("동일한 시간에 이미 예약이 되어 있습니다.");
+                    isSameTime = true;
+                    break;
+                }
+            }
+
+            if (!isSameTime) {  //왜 == null은 안되지? seat2[a - 1].isEmpty()
+                seat2[a - 1].add(RInfo);  //여기서 add(RInfo.name) 하면 안된다
                 System.out.println("예약 완료");
-            } else {
-                seat2[a - 1].add(name);
-                System.out.println("중복 예약됨.");
+                Collections.sort(seat2[a-1]);
+//                Collections.sort(seat2[a - 1], new Comparator<ReserveInfo>() {
+//                    @Override
+//                    public int compare(ReserveInfo o1, ReserveInfo o2) {
+//                        return o1.now.compareTo(o2.now);
+//                    }
+//                });
             }
 
             printArray(seat2);
@@ -39,12 +92,13 @@ class Reserve {
         }
     }
 
-    private void printArray(List<String>[] seats) {
+    private void printArray(List<ReserveInfo>[] seats) {
         for (int i = 0; i < seats.length; i++) {
             if (seats[i].isEmpty()) {
-                System.out.print((i + 1) + "번: 0 ");
+                System.out.println((i + 1) + "번: 0 ");
             } else {
-                System.out.print((i + 1) + "번: " + seats[i] + " ");
+                for(ReserveInfo info : seats[i])
+                    System.out.println((i + 1) + "번: 이름: " + info.name + " 예약 시간: " + info.now.format(formatter)); //분단위
             }
         }
         System.out.println();
